5c5f63cfe6e92d3afd7dfe32f9510261e7c2b9b6
Vinay Kumar Maheshwaram <vmaheshw@progress.com>
2026-03-10 21:04:49 +0530

    test: add sample file for copilot review

---
+++ sample.js
@@ -0,0 +1,12 @@
+﻿function helloWorld(){
+    console.log("hello")
+}
+
+var x = "mypassword123"
+
+function reallyLongFunctionThatDoesLotsOfThingsAndExceedsFiftyLines(a,b,c,d){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+}
